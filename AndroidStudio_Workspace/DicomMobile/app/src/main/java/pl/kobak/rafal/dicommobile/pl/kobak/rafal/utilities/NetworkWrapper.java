3c5ba93d81243f693d1cb006f24520eb4ca40128
Rafal Kobak <raf.kobak@gmail.com>
2016-05-12 00:13:24 +0200

    Obsluga siecie, czesc pierwsza

--- AndroidStudio_Workspace/DicomMobile/app/src/main/java/pl/kobak/rafal/dicommobile/pl/kobak/rafal/utilities/NetworkWrapper.java
+++ AndroidStudio_Workspace/DicomMobile/app/src/main/java/pl/kobak/rafal/dicommobile/pl/kobak/rafal/utilities/NetworkWrapper.java
@@ -4,17 +4,21 @@ import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
 
 /**
  * Created by Rafal on 2016-05-05.
  */
 public class NetworkWrapper
 {
+    static final String LABEL = "NetworkWrapper";
+
     public NetworkWrapper()
     {
         Log.d(LABEL, "Object of "
@@ -25,33 +29,43 @@ public class NetworkWrapper
     public void connect()
     {
         Log.d(LABEL, "Function connect has been called");
-        m_connectionThread = new Thread(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                try
-                {
-                    InetAddress l_serverAddr = InetAddress.getByName(SERVER_IP);
-                    Socket l_socket = new Socket(l_serverAddr, SERVER_PORT);
-                    PrintWriter l_out = new PrintWriter(l_socket.getOutputStream(), true);
-                    BufferedReader l_in = new BufferedReader(new InputStreamReader(l_socket.getInputStream()));
-                }
-                catch (UnknownHostException e)
-                {
-                    Log.d(LABEL, "Exception occurred: Unknown host: 192.168.1.6");
-                }
-                catch  (IOException e)
-                {
-                    Log.d(LABEL, "Exception occurred: No I/O");
-                }
-            }
-        });
-        m_connectionThread.start();
+        Thread l_connectionThread = new Thread(new ClientThread());
+        l_connectionThread.start();
     }
+}
 
-    private static final String LABEL = "NetworkWrapper";
+class ClientThread implements Runnable
+{
     private static final String SERVER_IP = "192.168.1.6";
     private static final int SERVER_PORT = 9878;
-    private Thread m_connectionThread;
+    static final String LABEL = "NetworkWrapper";
+
+    @Override
+    public void run()
+    {
+        try
+        {
+            InetAddress l_serverAddr = InetAddress.getByName(SERVER_IP);
+            Socket l_socket = new Socket(l_serverAddr, SERVER_PORT);
+            Log.d(LABEL, "Bla");
+
+            InputStreamReader l_in = new InputStreamReader(l_socket.getInputStream());
+            Log.d(LABEL, "Bla");
+            char[] buffer = new char[1024];
+            int bla = l_in.read(buffer);
+            Log.d(LABEL, "X");
+            for (int i = 0; i < 40; i++)
+            {
+               Log.d(LABEL, "rec: " + buffer[i]);
+            }
+        }
+        catch (UnknownHostException e)
+        {
+            Log.d(LABEL, "Exception occurred: Unknown host: 192.168.1.6");
+        }
+        catch (IOException e)
+        {
+            Log.d(LABEL, "Exception occurred: No I/O");
+        }
+    }
 }
